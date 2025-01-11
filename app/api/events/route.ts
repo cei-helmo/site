79913b3fbd49db9d20373147196cc49597d8d51f
Julien <julien.milants@gmail.com>
2025-01-11 11:41:48 +0100

    fix: fix du bug de event en prod, maintenant ça fonctionne

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from "next/server";
 import prisma from "@/src/utils/prisma/index";
 
@@ -5,21 +7,13 @@ export async function GET() {
   try {
     const events = await prisma.event.findMany({
       include: {
-        createdBy: {
-          select: { name: true },
-        },
+        createdBy: { select: { name: true } },
       },
-      orderBy: {
-        date: "desc",
-      },
+      orderBy: { date: "desc" },
     });
-
     return NextResponse.json(events);
   } catch (error) {
-    console.error("Error fetching events:", error); // LOG: Erreur serveur
-    return NextResponse.json(
-      { message: "Une erreur est survenue." },
-      { status: 500 },
-    );
+    console.error("Error fetching events:", error);
+    return NextResponse.json({ message: "Une erreur est survenue." }, { status: 500 });
   }
 }
