8f0b002eddaf5eda73b3d6a41db9b6deebbe07f6
Julien <julien.milants@gmail.com>
2025-03-20 16:24:41 +0100

    test: test ci avec portainer

---
+++ app/pages/status-venti/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/src/components/Navbar";
+
+export default function page() {
+  return (
+    <>
+        <Navbar/>
+        <div className="bg-white dark:bg-gray-800 min-h-screen flex flex-col justify-center items-center">
+            <h1 className="text-black dark:text-white">test</h1>
+        </div>
+    </>
+
+  )
+}
